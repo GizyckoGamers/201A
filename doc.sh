52cce340b172e847896dea3b181bb1a2ed6888ed
maciekMerkury <maciekmerkury2@gmail.com>
2022-07-04 10:05:33 +0200

    Changed from $BROWSER to xdg-open

--- doc.sh
+++ doc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-$BROWSER https://docs.google.com/document/d/14OeliWTIES97kI2jdlHxP4Etvy1XG9S9hAVEjimbhYU/edit?usp=sharing &>/dev/null &
+xdg-open https://docs.google.com/document/d/14OeliWTIES97kI2jdlHxP4Etvy1XG9S9hAVEjimbhYU/edit?usp=sharing &>/dev/null &
 
