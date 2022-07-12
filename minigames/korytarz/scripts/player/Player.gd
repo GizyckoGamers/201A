547692fbb2a44adec06039224f62a6ddb5ea4af9
Mateusz Soszyński <mateusz.soszynski@tuta.io>
2022-07-12 12:16:31 +0200

    add joystick to scene!

--- minigames/korytarz/scripts/player/Player.gd
+++ minigames/korytarz/scripts/player/Player.gd
@@ -10,21 +10,30 @@ func game_over():
 func _ready():
 	pass
 
+const joy_deadzone = 0.01
+var joystick := Vector2(0,0)
+
 func _physics_process(delta):
 	var direction = Vector2()
 	
 	if _is_running:
-		if Input.is_action_pressed("up"):
-			direction.y -= 1
-		if Input.is_action_pressed("down"):
-			direction.y += 1
-		if Input.is_action_pressed("right"):
-			direction.x += 1
-		if Input.is_action_pressed("left"):
-			direction.x -= 1
-		
-		direction = direction.normalized()
+		# Joysitck has higher priority than keyboard
+		if joystick == Vector2(0,0):
+			if Input.is_action_pressed("up"):
+				direction.y -= 1
+			if Input.is_action_pressed("down"):
+				direction.y += 1
+			if Input.is_action_pressed("right"):
+				direction.x += 1
+			if Input.is_action_pressed("left"):
+				direction.x -= 1
+			direction.normalized()
+		elif joystick.length() > joy_deadzone:
+			direction += joystick
 		
 		look_at(position + direction)
 		
 		direction = move_and_slide(direction * Constants.movespeed)
+
+func _on_Joystick_move_vector(move_vector):
+	joystick = move_vector
