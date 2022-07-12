056965b68063e486af7217aad3a94866029fd3bc
maciekMerkury <maciekmerkury2@gmail.com>
2022-07-12 19:51:12 +0200

    Completed enough of the 'game' to warrant pushing it

---
+++ minigames/rejser/scripts/kill_me.gd
@@ -0,0 +1,93 @@
+extends KinematicBody2D
+
+export var wheel_base: float = 70
+export var steering_angle: float = 15
+export var engine_power: float = 800
+export var friction: float = -0.9
+export var drag: float = -0.001
+export var braking: float = -450
+export var max_speed_reverse: float = 250
+export var slip_speed: float = 400
+export var traction_fast: float = 0.1
+export var traction_slow: float = 0.7
+export var max_gear: int = 5
+export var max_rpm: int = 5500
+var gear: int = 0
+var rpm: int = 0
+
+var acceleration := Vector2.ZERO
+var velocity := Vector2.ZERO
+var steer_direction
+var fully_stopping: bool = false
+
+func assert_or_print(lhs: float, rhs: float):
+	if is_equal_approx(lhs, rhs):
+		print("%s != %s" % [lhs, rhs])
+		assert(is_equal_approx(lhs, rhs))
+
+func _get_power(gear: int, rpm: int) -> float:
+	var x: float = rpm as float / 6000 as float
+	return (2*x)/(1.218 * log(gear + 1) * pow(3*x, x))
+	
+func _physics_process(delta):
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction()
+	calculate_steering(delta)
+	velocity += acceleration * delta
+	velocity = move_and_slide(velocity)
+
+func apply_friction():
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction
+	var drag_force = velocity * velocity.length() * drag
+	acceleration += drag_force + friction_force
+
+func get_input():
+	var turn = 0
+	if Input.is_action_pressed("ui_right"):
+		turn += 1
+	if Input.is_action_pressed("ui_left"):
+		turn -= 1
+	steer_direction = turn * deg2rad(steering_angle)
+#	if Input.is_action_pressed("ui_up"):
+#		acceleration = transform.x * engine_power
+	acceleration += transform.x * engine_power * Input.get_action_strength("ui_up")
+	acceleration += transform.x * braking * Input.get_action_strength("ui_down")
+#	if Input.is_action_pressed("ui_down") || Input.is_action_pressed("ui_accept"):
+#		acceleration = transform.x * braking
+	if Input.is_action_just_pressed("right_key"):
+		gear = clamp(gear + 1, -1, max_gear)
+		
+	if Input.is_action_just_pressed("left_key"):
+		gear = clamp(gear - 1, -1, max_gear)
+	
+	fully_stopping = Input.is_action_pressed("ui_accept")
+
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	var traction = traction_slow
+	if velocity.length() > slip_speed:
+		traction = traction_fast
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0:
+		velocity = velocity.linear_interpolate(new_heading * velocity.length(), traction)
+	if d < 0:
+		if !fully_stopping:
+			velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+		else:
+			velocity = Vector2.ZERO
+	rotation = new_heading.angle()
+	
+	
+func _ready():
+	assert_or_print(_get_power(1, 3709), 1)
+
+
+func _on_Area2D_body_exited(body: Node) -> void:
+	print("a cunt left the track: ", body)
---
+++ minigames/rejser/scripts/start_stop_gate.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+const player = preload("res://scripts/kill_me.gd")
+
+var time: float = 0.0
+var sec_1_time: float = -1.0
+var sec_2_time: float = -1.0
+var started := false
+
+func start_timer():
+	started = true
+
+func stop_timer():
+	started = false
+
+func as_nice_string(input: float) -> String:
+	var minutes := input / 60
+	var seconds := fmod(input, 60)
+	var milliseconds := fmod(input, 1) * 100
+	return "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
+
+func _process(delta: float):
+	if started:
+		time += delta
+
+func _on_main_gate_body_entered(body: Node) -> void:
+	if not body is player:
+		return;
+	
+	started = !started
+	
+	if !started:
+		sec_2_time = time - sec_1_time
+		print("total time: ", as_nice_string(time))
+		print("sec1 time: ", as_nice_string(sec_1_time))
+		print("sec2 time: ", as_nice_string(sec_2_time))
+
+func _on_sec_gate_1_body_entered(body: Node) -> void:
+	sec_1_time = time
