4050b8e4aa9a48fa0db387dfb6e7d4d5f644caa1
piotrprzybysz <piotr.przybysz.dev@gmail.com>
2022-07-13 11:41:30 +0200

    added player sprites

--- minigames/korytarz/scripts/player/Constants.gd
+++ minigames/korytarz/scripts/player/Constants.gd
@@ -7,14 +7,10 @@ const starting_position = Vector2(120, 280)
 
 const player_texture_links = [
 	"res://minigames/korytarz/sprites/player_textures/niemiec.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png", 
-#	"res://minigames/korytarz/sprites/player_textures/.png"
+	"res://minigames/korytarz/sprites/player_textures/KGG.png", 
+	"res://minigames/korytarz/sprites/player_textures/somsiad.png", 
+	"res://minigames/korytarz/sprites/player_textures/Szwajcar.png", 
+	"res://minigames/korytarz/sprites/player_textures/Soszynski.png", 
+	"res://minigames/korytarz/sprites/player_textures/skibicki.png", 
+	"res://minigames/korytarz/sprites/player_textures/kajtek.png"
 ]
